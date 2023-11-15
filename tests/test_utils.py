6076ac945f8017a561333d80044b6979c36ec8be
Maxim Kuznetsov <maxim_kuznetsov@bk.ru>
2023-11-15 11:52:25 +0300

    Add function test_format_data in 'tests/test_utils.py'

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from src.utils import sort_and_filter
+from src.utils import sort_and_filter, format_data
 
 
 def test_sort_and_filter():
@@ -10,3 +10,12 @@ def test_sort_and_filter():
     """
     assert type(sort_and_filter()) == list
     assert len(sort_and_filter()) == 5
+
+
+def test_format_data():
+    """
+    Тестируем функцию format_data на получение корректного формата
+    """
+    assert format_data("2018-12-20T16:43:26.929246") == "20.12.2018"
+    assert format_data("2018-09-12T21:27:25.241689") == "12.09.2018"
+    assert format_data("2019-12-07T06:17:14.634890") == "07.12.2019"
