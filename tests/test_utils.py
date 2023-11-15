94fe39e7086d819a661549dd0c24262b7305251c
Maxim Kuznetsov <maxim_kuznetsov@bk.ru>
2023-11-15 11:57:01 +0300

    Add function test_card_format in 'tests/test_utils.py'

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from src.utils import sort_and_filter, format_data
+from src.utils import sort_and_filter, format_data, card_format
 
 
 def test_sort_and_filter():
@@ -19,3 +19,14 @@ def test_format_data():
     assert format_data("2018-12-20T16:43:26.929246") == "20.12.2018"
     assert format_data("2018-09-12T21:27:25.241689") == "12.09.2018"
     assert format_data("2019-12-07T06:17:14.634890") == "07.12.2019"
+
+
+def test_card_format():
+    """
+    Тестируем функцию card_format на получение корректного формата
+    при подаче на ввод счетов в разных вариантах
+    """
+    assert card_format("МИР 8665240839126074") == "МИР 8665 24** **** 6074"
+    assert card_format("Maestro 3000704277834087") == "Maestro 3000 70** **** 4087"
+    assert card_format("Visa Classic 2842878893689012") == "Visa Classic 2842 87** **** 9012"
+    assert card_format("Счет 35158586384610753655") == "Счет **3655"
